8547df20f2c3c69a694eb1f04b377c5d96939eea
vijaykharde <vijay.kharde@gmail.com>
2020-05-16 02:27:01 +0530

    Angular Webpack Demo Added
    Angular Webpack Demo Added

---
+++ angular-webpack/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+const styles = './app.component.css';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  //template: `<b>Welcome to Angular</b>`,
+  styleUrls: [styles]
+})
+export class AppComponent {
+  title = 'angular-webpack';
+}
